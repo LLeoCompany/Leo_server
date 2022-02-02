197cc42840899578696e3bdd7dbfb6e71266d64a
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-02-02 13:33:17 +0900

    node express

---
+++ App.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+//전역변수(속성)
+app.set('port',process.env.PORT || 3000);
+
+
+app.use((req,res)=>{
+    console.log("공통 실행")
+    next(); //미들웨어는 next 를 해야만 다음 챕터로 넘어간다
+})
+
+app.get('/',(req,res)=>{
+ res.sendFile(path.join(__dirname,'./index.html'));
+});
+app.post('/',(req,res)=>{
+    res.send('hello world');
+   });
+   app.get('/about',(req,res)=>{
+    res.send('hello world');
+   });
+
+app.get('/category/:name',(req,res)=>{
+    res.send(`hello ${req.params.name}`);
+})
+
+
+app.listen(app.get('port'),()=>{
+    console.log('express server start')
+});

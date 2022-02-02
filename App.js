50405a9c74cac047a0acc092010dde464cd4966a
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-02-02 14:57:54 +0900

    error 미들웨어 커스텀

--- App.js
+++ App.js
@@ -6,9 +6,11 @@ const path = require('path');
 app.set('port',process.env.PORT || 3000);
 
 
-app.use('/about',(req,res,next)=>{
+app.use((req,res,next)=>{
     console.log("공통 실행")    
     next(); //미들웨어는 next 를 해야만 다음 챕터로 넘어간다
+},(req,res,next)=>{
+    throw new Error("에러 발생")
 })
 
 app.get('/',(req,res)=>{
@@ -25,9 +27,15 @@ app.get('/category/:name',(req,res)=>{
     res.send(`hello ${req.params.name}`);
 })
 
+//에러 커스텀 
+app.use((req,res,next)=>{
+   res.status(404).send('404에러') //에러 표시 를 커스텀 화 시킬수 잇다
+})
+
 //error 미들웨어는 생략하면 안됨 4개 다 필수
 app.use((err,req,res,next) => {
     console.log("error....");
+    res.send("error 발생")
 })
 
 

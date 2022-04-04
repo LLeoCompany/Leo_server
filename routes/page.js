c72b52fdc8666503f84c0523d9741372e620568b
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-04-04 19:27:07 +0900

    mysql 연결

--- routes/page.js
+++ routes/page.js
@@ -1,21 +1,22 @@
-const express =require('express');
+const express = require('express');
 
 const router = express.Router();
 
 router.use((req,res,next)=>{
-    res,locals.user = null;
+    res.locals.user = null;
     res.locals.followerCount = 0;
-    res. locals.followingCount = 0;
+    res.locals.followingCount = 0;
     res.locals.followerIdList = [];
     next();
 });
 
-router.get('/profile',(req,res)=>{
-    res.render('profile',{title:'내 정보 - NodeBird'});
+
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
-router.get('/join',(req,res)=>{
-    res.render('join',{title:'회원가입 - NodeBird',});
+router.get('/join', (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
 });
 
 router.get('/',(req,res,next)=>{
@@ -26,4 +27,4 @@ router.get('/',(req,res,next)=>{
     });
 });
 
-module.exports = router;
+module.exports =  router;

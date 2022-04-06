ebc91bd47e1476d13ee713b73801aed75059a947
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-04-06 19:46:32 +0900

    passport 설정

--- App.js
+++ App.js
@@ -5,14 +5,17 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const passport = require('passport');
+
 
 //설정파일-이후
 dotenv.config();
 const pageRouter = require('./routes/page');
 const {sequelize} = require('./models')
-
+const pssportConfig = require('./passport');
 
 const app = express();
+pssportConfig(); //패스포트 설정
 
 app.set('port',process.env.PORT || 8001);
 app.set('view engine','html');
@@ -45,6 +48,9 @@ app.use(session({
     secure:false,
   },
 }));
+
+app.use(passport.initialize);
+app.use(passport.session());
 
 app.use('/',pageRouter);
 

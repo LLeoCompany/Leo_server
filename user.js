63d519342d40d8becb669579dfd92d7f8548e0f7
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-04-11 19:23:12 +0900

    npm

---
+++ user.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+let participants = [
+  {
+    id: 1,
+    nation: "test1",
+  },
+  {
+    id: 2,
+    nation: "test1",
+  },
+  {
+    id: 3,
+    nation: "test1",
+  },
+];

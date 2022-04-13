963c4f17a97cfc54213528abbb037653a6ad1bfb
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-04-13 19:40:28 +0900

    node port

--- src/server.js
+++ src/server.js
@@ -3,4 +3,25 @@ import express from "express";
 
 const app = express();
 
+const PORT = 4000;
+
+const Home = (req,res) =>{
+    // return res.end();
+    return res.send("love")
+}
+
+const handleLogin = (req,res) => {
+    return res.send("login auth")
+}
+
+app.get("/",Home)
+ 
+app.get("/login",handleLogin)
+
+
+const handleListening =()=>{
+ console.log(`server open ${PORT} `)   
+}
+
+app.listen(4000,handleListening);
 

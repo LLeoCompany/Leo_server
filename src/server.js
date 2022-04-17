0f3c20750dbec4777c168ae11861a2cb77e7835a
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-04-17 17:54:02 +0900

    node middleware

--- src/server.js
+++ src/server.js
@@ -5,9 +5,17 @@ const app = express();
 
 const PORT = 4000;
 
-const gosipMiddleWare = (req,res,next) =>{
+const logger = (req,res,next) =>{
     console.log(`${req.method} ${req.url}`);
     next();
+}
+
+const privateMiddleWare =(req,res,next)=>{
+    if(req.url === "/private"){
+        res.send(`<h1>not router </h1>`)
+    }
+    console.log("pass pss")
+        next();
 }
 
 
@@ -16,13 +24,19 @@ const Home = (req,res,next) =>{
     return res.send("<h1>love</h1>")
 }
 
+const product =(req,res)=>{
+    return res.send("welcome to product filed")
+}
+
 const handleLogin = (req,res) => {
     return res.send("login auth")
 }
 
-app.use(gosipMiddleWare);
+app.use(privateMiddleWare);
+app.use(logger);
+
 app.get("/",Home)
- 
+app.get("/product",product )
 app.get("/login",handleLogin)
 
 

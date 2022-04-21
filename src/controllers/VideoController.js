1a6d9f00ed94686fd24d5f7c8bbd3d39ce30a9c0
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-04-21 22:39:06 +0900

    pug mvp style

--- src/controllers/VideoController.js
+++ src/controllers/VideoController.js
@@ -1,8 +1,40 @@
 
 
-export const trending = (req,res)=> {
-    return res.render("home");
+const fakeUser = {
+    username : "sunghwan",
+    loggedIn:false,
 }
+
+export const trending = (req,res)=> {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        }
+    ];
+    return res.render("home",{pageTitle:"HOME",videos})
+}
+
 
 export const see = (req,res)=>{
     return res.render("watch");
